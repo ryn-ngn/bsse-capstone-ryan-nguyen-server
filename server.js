03e9331e24026d1470259d08163f2d7eecd9d30f
Ryan Nguyen <ryan.ngn@outlook.com>
2024-04-13 15:44:30 -0400

    created project structure and register route handler
    create migration for users table and cars table, seed cars data
    
    compact data file

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+const jsonSecretKey = process.env.JWT_SECRET;
+const PORT = process.env.PORT || 8080;
+const BASE_URL = process.env.BASE_URL || "http://localhost";
+const { getToken } = require("./utils/helper");
+
+const landing = require("./routes/landing");
+// const collection = require("./routes/collection");
+// const serviceRecord = require("./routes/service-record");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// apply authentication rule if routes being called are not login & register
+app.use((req, res, next) => {
+  if (req.url === "/api/register" || req.url === "/api/login") {
+    next();
+  } else {
+    const token = getToken(req);
+
+    if (token) {
+      if (jwt.verify(token, jsonSecretKey)) {
+        req.decode = jwt.decode(token);
+        next();
+      } else {
+        res.status(403).json({ error: "Not Authorized." });
+      }
+    } else {
+      res.status(403).json({ error: "No token. Unauthorized." });
+    }
+  }
+});
+
+// routes
+app.use("/api", landing);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .send(
+      "An unexpected error occurred while processing your request. Please try again later"
+    );
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on ${BASE_URL}:${PORT}`);
+});
---
+++ utils/helper.js
@@ -0,0 +1,62 @@
+const bcrypt = require("bcrypt");
+const JWT_SALT_ROUNDS = process.env.JWT_SALT_ROUNDS;
+
+const getToken = (req) => {
+  if (!req.headers.authorization) {
+    return;
+  } else {
+    return req.headers.authorization.split(" ")[1];
+  }
+};
+
+// string input needs to have minimum 2 character
+const isStringInputValid = (inputArr) => {
+  for (input of inputArr) {
+    if (!input || input.length < 2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// hash password input received
+const hashPassword = async (inputPassword) => {
+  try {
+    const salt = await bcrypt.genSalt(JWT_SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(inputPassword, salt);
+    return hashedPassword;
+  } catch (error) {
+    console.error("Unable to harsh input password: ", error);
+  }
+};
+
+// isUserNameAvailable
+const isUserNameAvailable = async(userName) => {
+  try {
+    const users = await getAllUsers();
+    // attempt to look for userName in existing data
+    const matchingUserName = users.find(user => user.userName === userName)
+    // return the negated find result
+    return !matchingUserName;
+  } catch (error) {
+
+  }
+}
+
+// getAllUsers
+const getAllUsers = async () => {
+  try {
+    const users = await knex("users");
+    return users;
+  } catch (error) {
+    console.error("Error fetching users data", error);
+    throw new Error("Error fetching users data");
+  }
+};
+
+module.exports = {
+  getToken,
+  hashPassword,
+  isStringInputValid,
+  isUserNameAvailable,
+};
