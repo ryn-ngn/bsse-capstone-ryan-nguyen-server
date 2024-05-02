c970baf2c8405c1e36efca0b015ca64c0572172d
Ryan Nguyen <ryan.ngn@outlook.com>
2024-05-02 10:30:44 -0400

    bug fix

--- routes/cars.js
+++ routes/cars.js
@@ -7,6 +7,11 @@ const {
   getCarFromMakeModelYear,
 } = require('../utils/cars-controller');
 
+// GET /api/cars/:carId
+// retrieve basic car info from database
+// expected params: { carId }
+router.route('/:carId').get(getBasicCarInfoById);
+
 // GET car makes
 router.route('/make').get(getAllCarMakes);
 
@@ -21,10 +26,5 @@ router.route('/:make/:model').get(getCarYearFromMakeModel);
 // GET cars by make model year
 // expect body: { make, model }
 router.route('/:make/:model/:year').get(getCarFromMakeModelYear);
-
-// GET /api/cars/:carId
-// retrieve basic car info from database
-// expected params: { carId }
-router.route('/:carId').get(getBasicCarInfoById);
 
 module.exports = router;
